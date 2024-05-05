46d6d8e922dab5ebbdc7d9ba2356b66b01485de3
Anton <vanasscheanton@gmail.com>
2024-05-05 17:20:59 +0200

    [confiscan] Set version to 0.8-devel

--- src/tool/confiscan.sh
+++ src/tool/confiscan.sh
@@ -25,7 +25,7 @@ declare -r BYELLOW='\e[1;33m'      # yellow
 declare -r BBLUE='\e[1;34m'        # blue
 
 # Variables
-VERSION="0.7-devel"
+VERSION="0.8-devel"
 NAME="ConfiScan"
 SCRIPT_NAME="${0##*/}"
 HOSTNAME="$(cat "/proc/sys/kernel/hostname")"
